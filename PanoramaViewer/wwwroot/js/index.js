21786620e5567839f58b0f41a96f08686df62637
Darrell Dumas <ddumas@enstoa.com>
2022-05-17 12:18:42 -0400

    Removed built assets from repo.

--- PanoramaViewer/wwwroot/js/index.js
+++
@@ -1,4 +0,0 @@
-import { getDotNetRef, init, animate, loadPanorama, addPointOfInterest, removePointOfInterest, refreshPointsOfInterest } from './panorama-viewer.js';
-
-export { getDotNetRef, init, animate, loadPanorama, addPointOfInterest, removePointOfInterest, refreshPointsOfInterest }
-
